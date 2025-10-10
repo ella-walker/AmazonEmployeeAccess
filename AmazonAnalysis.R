e06a68309bd8e6e3dee02594ccf4003659fbe052
ella-walker <wella2@byu.edu>
2025-10-10 12:29:05 -0600

    updated penalized logistic regression

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -8,6 +8,7 @@ library(skimr)
 library(dplyr)
 library(DataExplorer)
 library(corrplot)
+library(embed)
 
 # Read in test and training data
 amazon <- vroom("~/Documents/STAT 348/AmazonEmployeeAccess/archive/train.csv") |>
@@ -36,7 +37,8 @@ plot_correlation(amazon)
 amazon_recipe <- recipe(ACTION ~ ., data = amazon) |>
   step_mutate_at(all_predictors(), fn = factor) |>
   step_other(all_nominal_predictors(), threshold = .001) |>
-  step_dummy(all_nominal_predictors())
+  step_lencode_glm(all_nominal_predictors(), outcome = vars(ACTION)) |>
+  step_normalize(all_predictors())
 
 bake(prep(amazon_recipe), amazon)
 
@@ -70,3 +72,42 @@ vroom_write(kaggle_submission,
             delim = ",")
 
 
+##
+### Penalized Regression 
+##
+
+penalized_mod <- logistic_reg(mixture= tune(), penalty = tune()) |>
+  set_engine("glmnet")
+
+penalized_wf <- workflow() |>
+  add_recipe(amazon_recipe) |>
+  add_model(penalized_mod)
+
+penalized_tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon, v = 5, repeats = 1)
+
+penalized_CV_results <- penalized_wf |>
+  tune_grid(resamples = folds,
+            grid = penalized_tuning_grid,
+            metrics = metric_set(roc_auc))
+
+penalized_bestTune <- penalized_CV_results |>
+  select_best(metric = "roc_auc")
+
+penalized_final_wf <-
+  penalized_wf |>
+  finalize_workflow(penalized_bestTune) |>
+  fit(data = amazon)
+
+penalized_preds <- penalized_final_wf |>
+  predict(new_data = testData, type = "prob") |>
+  bind_cols(testData) |>
+  rename(ACTION = .pred_1) |>
+  select(id, ACTION)
+
+vroom_write(penalized_preds,
+            file = "~/Documents/STAT 348/AmazonEmployeeAccess/penalized_logistic.csv",
+            delim = ",")
